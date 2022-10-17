aab25c552bff04679f82dabff03aff7324702822
quothbonney <jackie.d.carson@gmail.com>
2022-10-16 22:34:45 -0500

    No segfaults in the no-intersection reader

--- src/rqs/rqsDataBlock.cpp
+++ src/rqs/rqsDataBlock.cpp
@@ -19,21 +19,34 @@ rqsDataBlock::rqsDataBlock(int id, int posX, int posY,
 void rqsDataBlock::readFromRaster() {
     // Possibly GNU compiler bug (???) pointer to vector is a vector?, this appears to work though.
     RasterQuery::geoTransformData raster = m_rqsDataInfo[0][m_origin.r];
+
+    // Check if there are intersections and read from the raster based on which intersections there are (4 options)
     bool xIntersections, yIntersections;
+    xIntersections = (m_origin.x + BLOCK_SIZE > raster.r_xSize);
+    yIntersections = (m_origin.y + BLOCK_SIZE > raster.r_ySize);
 
-    xIntersections = (m_origin.x + BLOCK_SIZE < raster.r_xSize);
-    yIntersections = (m_origin.y + BLOCK_SIZE < raster.r_ySize);
 
-    if(!xIntersections && !yIntersections) {
+    if(!xIntersections && !yIntersections) { // If no intersections
         for(int row = 0; row < BLOCK_SIZE; ++row) {
-            m_rqsCallOrder[0][m_origin.r]->RasterIO(GF_Read,
-                                                    m_origin.x, m_origin.y + row,
-                                                    BLOCK_SIZE, 1,
-                                                    &_spBlock[row],
-                                                    BLOCK_SIZE, 1,
-                                                    GDT_Int32,
-                                                    0, 0
-            );
+            // Create buffer; if the raster is undefined, the buffer is set equal to the value 0,
+            // Else, the raster is read normally
+            int* buffer = new int[BLOCK_SIZE];
+            auto t = m_rqsCallOrder[0][m_origin.r];
+            if(t == nullptr)
+                buffer[BLOCK_SIZE] = { 0 };
+            else {
+                t->RasterIO(GF_Read,
+                            m_origin.x, m_origin.y + row,
+                            BLOCK_SIZE, 1,
+                            buffer,
+                            BLOCK_SIZE, 1,
+                            GDT_Int32,
+                            0, 0
+                );
+            }
+            // copy data to smart pointer
+            memcpy(_spBlock[row].get(), buffer, BLOCK_SIZE * sizeof(int));
+            delete[] buffer;
         }
     }
 }
